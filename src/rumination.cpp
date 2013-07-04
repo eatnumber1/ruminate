71d27108402104bc7d60522805855452953c71de
Russell Harmon <russ@eatnumber1.com>
2013-07-04 10:35:14 -0700

    Child now starts automatically.

--- src/rumination.cpp
+++ src/rumination.cpp
@@ -12,8 +12,15 @@
 #include <glib.h>
 
 #include <unistd.h>
+#include <spawn.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+#include <sys/wait.h>
 
 #define die_unless(...)
+
+#define RUMINATION_DEBUGGER_CONTROLLER_PATH "./src/dbgsvr.py"
 
 struct Rumination {
 	Ice::CommunicatorPtr communicator;
@@ -21,6 +28,7 @@ struct Rumination {
 	Ruminate::DebuggerPrx debugger;
 	Ice::AsyncResultPtr arp;
 	// TODO: Deal with bad states
+	GPid child_pid;
 };
 
 G_BEGIN_DECLS
@@ -31,8 +39,26 @@ void rumination_hit_breakpoint() {
 	asm("");
 }
 
+static void setup_env() {
+	setenv("PYTHONPATH", "ice:src:.:/usr/local/lib/python2.7/site-packages", true);
+}
+
+static bool fork_child( Rumination *rum, GError **err ) {
+	size_t len = strlen(RUMINATION_DEBUGGER_CONTROLLER_PATH) + 1;
+	char controller_path[len];
+	memcpy(controller_path, RUMINATION_DEBUGGER_CONTROLLER_PATH, len);
+	char *argv[] = {
+		controller_path,
+		NULL
+	};
+	return g_spawn_async(NULL, argv, NULL, (GSpawnFlags) 0, (GSpawnChildSetupFunc) &setup_env, NULL, &rum->child_pid, err);
+}
+
 Rumination *rumination_new( int *argc, char *argv[], GError **err ) {
 	Rumination *ret = new Rumination();
+	if( !fork_child(ret, err) ) return NULL;
+	// Give python time to start up
+	sleep(1); // TODO
 	ret->communicator = Ice::initialize(*argc, argv);
 	auto factory_proxy = ret->communicator->stringToProxy("DebuggerFactory:default -p 1024");
 	ret->factory = Ruminate::DebuggerFactoryPrx::checkedCast(factory_proxy);
@@ -57,6 +83,8 @@ void rumination_delete( Rumination **rum ) {
 	Rumination *r = *rum;
 	r->factory->shutdown();
 	r->communicator->destroy();
+	// TODO: Check child return code
+	waitpid(r->child_pid, NULL, 0);
 	delete r;
 	*rum = NULL;
 }
