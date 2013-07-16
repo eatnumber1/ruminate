ea6fbd4e19652b26a9427d9e40dc654226f65931
Russell Harmon <russ@eatnumber1.com>
2013-07-16 03:03:03 -0700

    Add a test to test signal delivery.

--- test/main.c
+++ test/main.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <signal.h>
 
 typedef struct SubprocessTestFunction {
 	void (*parent)( int );
@@ -16,20 +17,9 @@ static void assert_child_success( int stat ) {
 	g_assert(WEXITSTATUS(stat) == 0);
 }
 
-static void test_rumination_init_subprocess() {
-	GError *err = NULL;
-	int argc = 1;
-	char *argv[] = { g_get_prgname() };
-
-	rumination_init(&argc, argv, &err);
-	g_assert_no_error(err);
-
-	rumination_destroy(&err);
-	g_assert_no_error(err);
-}
-
-static void test_rumination_init( int stat ) {
-	assert_child_success(stat);
+static void assert_child_fail_signal( int stat, int sig ) {
+	g_assert(WIFSIGNALED(stat));
+	g_assert(WTERMSIG(stat) == sig);
 }
 
 static void test_with_subprocess( SubprocessTestFunction *sptf ) {
@@ -45,6 +35,41 @@ static void test_with_subprocess( SubprocessTestFunction *sptf ) {
 	}
 }
 
+static void do_rumination_init() {
+	GError *err = NULL;
+	int argc = 1;
+	char *argv[] = { g_get_prgname() };
+
+	rumination_init(&argc, argv, &err);
+	g_assert_no_error(err);
+}
+
+static void do_rumination_destroy() {
+	GError *err = NULL;
+
+	rumination_destroy(&err);
+	g_assert_no_error(err);
+}
+
+static void test_rumination_init_subprocess() {
+	do_rumination_init();
+	do_rumination_destroy();
+}
+
+static void test_rumination_init( int stat ) {
+	assert_child_success(stat);
+}
+
+static void test_signal_delivery_subprocess() {
+	do_rumination_init();
+	kill(getpid(), SIGINT);
+	g_assert(false);
+}
+
+static void test_signal_delivery( int stat ) {
+	assert_child_fail_signal(stat, SIGINT);
+}
+
 int main( int argc, char **argv ) {
 	g_test_init(&argc, &argv, NULL);
 
@@ -53,6 +78,13 @@ int main( int argc, char **argv ) {
 		.child = test_rumination_init_subprocess
 	};
 	g_test_add_data_func("/start", &test_rumination_init_data, (GTestDataFunc) test_with_subprocess);
+
+
+	SubprocessTestFunction test_signal_delivery_data = {
+		.parent = test_signal_delivery,
+		.child = test_signal_delivery_subprocess
+	};
+	g_test_add_data_func("/signal", &test_signal_delivery_data, (GTestDataFunc) test_with_subprocess);
 
 	g_test_run();
 }
